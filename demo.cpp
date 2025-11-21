297582776d50d24eae12c97dd60ca001614869b4
rohiniwanjare <rohiniwanjare5@gmail.com>
2025-11-21 14:16:37 +0530

    bas atach suru kel

---
+++ demo.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    //how to declare vectors
+    vector<int>arr;
+    arr.push_back(5);
+    arr.push_back(1);
+    arr.push_back(6);
+    arr.push_back(7);
+    arr.push_back(2);
+
+    //print 
+    for(int ele : arr){
+        cout<<ele<<" ";
+    }
+
+}
